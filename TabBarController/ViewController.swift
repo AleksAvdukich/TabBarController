e12cd91ad26bc0e475790bfd357381ca636a5e9d
Aleksandr Avdukich <avdukichsanel@mail.ru>
2018-09-24 02:44:28 +0300

    Initial Commit

---
+++ TabBarController/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  TabBarController
+//
+//  Created by Aleksandr Avdukich on 24.09.2018.
+//  Copyright © 2018 Sanel Avdukich. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
